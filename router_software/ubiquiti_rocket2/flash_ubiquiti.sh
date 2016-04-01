08cf2d1b36d9bed27a20449fc0945ccbbe4cf19c
Abraham Quintero <abrahamq@mit.edu>
2016-04-01 14:51:17 -0700

    updated hamnet firmware address

--- router_software/ubiquiti_rocket2/flash_ubiquiti.sh
+++ router_software/ubiquiti_rocket2/flash_ubiquiti.sh
@@ -1,6 +1,6 @@
 # © 2015 Massachusetts Institute of Technology
 
-wget http://www.broadband-hamnet.org/download/firmware/ubnt/bbhn-3.0.0-ar71xx-generic-ubnt-rocket-m-squashfs-factory.bin
+wget http://broadband-hamnet.org/download/firmware/ubnt/300/bbhn-3.0.0-ar71xx-generic-ubnt-rocket-m-squashfs-factory.bin
 
 echo "Opening Firefox, username/password: admin/admin"
 
