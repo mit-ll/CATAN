53e72bafedded4ea5c28a6b805f9dc33765bee0a
Chad Spensky <cspensky@cs.ucsb.edu>
2016-04-01 15:11:41 -0700

    Fixed the check wifi script.

--- catan-services_1.0/scripts/check_wifi.sh
+++ catan-services_1.0/scripts/check_wifi.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/env bash
 #/sbin/ifconfig mon.wlan0 >/dev/null || (sudo service hostapd restart && sudo service isc-dhcp-server restart)
+
 # Check if gedit is running
+ip=$(/sbin/ifconfig wlan0 | grep -oP "(?<=inet addr:).*?(?=Bcast)" | sed -r 's/ //g')
+
+# Make sure our IP is set properly
+if [ "$ip" != "192.168.2.1" ]
+then
+    echo "`date -u`: Interface went down!  Restarting! ($ip != 192.168.2.1)"
+    sudo ifconfig wlan0 down
+    sudo ifconfig wlan0 up
+    sudo ifconfig wlan0 192.168.2.1
+    sudo service hostapd restart
+    sudo service isc-dhcp-server restart
+    exit
+fi
+
+# Is the DHCPD server up?
 if pgrep "dhcpd" > /dev/null
 then
-    sudo service isc-dhcp-server restart
+    # Do Nothing
+    echo "" > /dev/null
 else
+    echo "`date -u`: DHCPD was down!  Restarting!"
+    sudo ifconfig wlan0 192.168.2.1
     sudo service isc-dhcp-server restart
 fi
